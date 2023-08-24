84a1e63b151bfdba30792229e447dfbaa859c0cc
Clifford18 <cliffordjohn.cj@gmail.com>
2023-08-24 13:41:39 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,11 @@
 void print_before_main(void) __attribute__ ((constructor));
 
 /**
- * print_before_main - A constructor function that prints a message before main.
+ * print_before_main - A constructor
+ * function that prints a message before main.
  */
 void print_before_main(void)
 {
-    printf("Preparing to enter the realm of main function.\n");
-    printf("Carrying my abode upon my dorsal expanse!\n");
+	printf("Preparing to enter the realm of main function.\n");
+	printf("Carrying my abode upon my dorsal expanse!\n");
 }

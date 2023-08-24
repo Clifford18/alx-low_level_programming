1813806584fd56c558d56326e5322304750ef7e6
Clifford18 <cliffordjohn.cj@gmail.com>
2023-08-24 16:56:20 -0700

    first advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-/***
- *  Declare a constructor function to print a message before the main function.
- */
-void executeBeforeMain(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
- * executeBeforeMain - This function is
- *automatically executed before main,
- *printing a couple of lines.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void executeBeforeMain(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }

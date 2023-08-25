010875965b76abf483b969da2a4542543c8b335a
Clifford18 <cliffordjohn.cj@gmail.com>
2023-08-24 17:48:29 -0700

    updates

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,9 @@
-    format db 'Hello, World!',0
-
-    global main
-    extern printf
-
+	global   main
+	  extern    printf
 main:
-    mov edi, format
-    xor eax, eax
-    call printf
-    xor eax, eax
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
